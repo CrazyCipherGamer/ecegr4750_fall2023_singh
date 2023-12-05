781bb4ef1f625cdbd06c60aa0a9e53d3e4d0d1f8
CrazyCipherGamer5827 <raja.sell5827@gmail.com>
2023-12-05 00:50:09 -0800

    Implement process_images function to process images

--- paramveer_singh/utils.py
+++ paramveer_singh/utils.py
@@ -1,8 +1,20 @@
 import numpy as np
 import pandas as pd
+from PIL import Image
 
 # define function to standardize numerical features of dataframe
 def standardize_numeric(series: pd.Series, use_log: bool = False) -> pd.Series:
     if use_log:
         series = np.log(series)
     return (series - np.mean(series))/np.std(series)
+
+# define function to convert PIL images into correct format for exporting and model training
+def process_images(image_filenames: pd.Series) -> np.ndarray:
+    
+    images = []
+
+    for image_path in image_filenames:
+        im = Image.open('data/images/' + image_path)
+        images.append(np.moveaxis(np.asarray(im), 2, 0))
+    
+    return np.asarray(images)
