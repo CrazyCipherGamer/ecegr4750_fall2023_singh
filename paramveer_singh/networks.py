4af92a68542e028ef378ea46994ad8f564e3d83b
CrazyCipherGamer5827 <raja.sell5827@gmail.com>
2023-12-05 13:59:16 -0800

    Update networks.py to use correct number of linear inputs

--- paramveer_singh/networks.py
+++ paramveer_singh/networks.py
@@ -6,17 +6,17 @@ import torch.nn as nn
 class CNNRegression(nn.Module):
     def __init__(self):
         super(CNNRegression, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=16, kernel_size=10, stride=10, padding=0)
-        self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=4, stride=2, padding=0)
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=16, kernel_size=2, stride=2, padding=0)
+        self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=2, stride=2, padding=0)
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
         self.relu = nn.ReLU()  # Adding ReLU activation
 
         # Adjusting the fully connected layer input size based on the updated feature map dimensions
-        self.fc1 = nn.Linear(32 * 50 * 50, 1)  # Assuming two pooling layers reducing to 50x50
+        self.fc1 = nn.Linear(32 * 16 * 16, 1)
 
     def forward(self, x):
         x = self.pool(self.relu(self.conv1(x)))  # Applying ReLU after the first convolution and pooling
         x = self.pool(self.relu(self.conv2(x)))  # Applying ReLU after the second convolution and pooling
-        x = x.view(-1, 32 * 50 * 50)  # Flatten the output for the fully connected layer
+        x = x.view(-1, 32 * 16 * 16)  # Flatten the output for the fully connected layer
         x = self.fc1(x)
         return x
