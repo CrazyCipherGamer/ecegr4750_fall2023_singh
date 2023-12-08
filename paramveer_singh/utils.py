8e475ffd33a42d6a5b86d4c56972005a57d81ccf
CrazyCipherGamer5827 <raja.sell5827@gmail.com>
2023-12-08 00:00:54 -0800

    Update utils.py to process and return images as torch tensors

--- paramveer_singh/utils.py
+++ paramveer_singh/utils.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 from PIL import Image
+import torch
 
 # define function to standardize numerical features of dataframe
 def standardize_numeric(series: pd.Series, use_log: bool = False) -> pd.Series:
@@ -11,7 +12,7 @@ def standardize_numeric(series: pd.Series, use_log: bool = False) -> pd.Series:
 # define function to convert PIL images into correct format for exporting and model training
 # Images are resized down to 64 x 64 pixels so that image processing does not exceed memory
 # This further reduces time for image processing
-def process_images(image_filenames: pd.Series) -> np.ndarray:
+def process_images(image_filenames: pd.Series) -> torch.Tensor:
     
     images = []
 
@@ -20,4 +21,4 @@ def process_images(image_filenames: pd.Series) -> np.ndarray:
         im = im.resize((64, 64))
         images.append(np.moveaxis(np.asarray(im), 2, 0))
     
-    return np.asarray(images)
+    return torch.Tensor(np.asarray(images))
